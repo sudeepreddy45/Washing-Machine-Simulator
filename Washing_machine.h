e06606afabb6015308c74967076e014f1b68bacb
Sudeep Reddy Pedaballe <sudeepreddy_pedaballe@srmap.edu.in>
2025-07-22 20:37:57 +0530

    Create Washing_machine.h

---
+++ Washing_machine.h
@@ -0,0 +1,13 @@
+#ifndef WASHING_MACHINE_HEADER_H
+#define WASHING_MACHINE_HEADER_H
+
+void power_on_screen(void);
+void clear_screen(void);
+void washing_program_display( unsigned char key);
+void water_level_screen( unsigned char key);
+void set_time(void);
+void run_program(unsigned  char key);
+void door_status_check(void);
+
+
+#endif /* WASHING_MACHINE_HEADER_H */
